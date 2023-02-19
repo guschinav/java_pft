f3c497abf338ddf9e68e9239a862536b169bfe55
guschinav <sasha.gushchin.99@gmail.com>
2023-02-19 15:17:09 +0300

    Переделан тест, в соответствии с предыдущими исправлениями

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -8,8 +8,13 @@ public class PointTests {
 
     @Test
 
-    public void testDistance (){
-        Distance4 p1 = new Distance4(-2.4, -8.8, 4.7, 9.8);
-        Assert.assertEquals(p1.distance(), 19.9090431713832);
+    public void testPoint4 (){
+        Point4 p1 = new Point4();
+        Point4 p2 = new Point4();
+        p1.x = -2.0;
+        p1.y = -8.0;
+        p2.x = 4.0;
+        p2.y = 9.0;
+        Assert.assertEquals(p1.distance(p2), 18.027756377319946);
     }
 }
