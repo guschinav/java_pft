4f91d7d92021899451ed4dca1b98b3643e10bed1
guschinav <sasha.gushchin.99@gmail.com>
2023-02-25 12:03:45 +0300

    Метод и функция реализована в одном классе

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -15,10 +15,20 @@ public class Point {
 
         System.out.println("Расстояние между точками Point 1(" + p1.x + ";" + p1.y + ") и Point 2(" + p2.x + ";" + p2.y + ") = " + distance(p1, p2));
 
+        //Использование метода
+        System.out.println("Расстояние между точками Point 1(" + p1.x + ";" + p1.y + ") и Point 2(" + p2.x + ";" + p2.y + ") = " + p1.distance(p2));
+
     }
 
+    //Функция
     public static double distance (Point p1, Point p2) {
 
         return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    }
+
+    //Метод
+    public double distance (Point p2) {
+
+        return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
     }
 }
