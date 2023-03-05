78f8b97af8e1ac67a9c11a045efe3cb1fc64a391
guschinav <sasha.gushchin.99@gmail.com>
2023-03-05 16:04:00 +0300

    Update GroupCreationTests.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -2,12 +2,13 @@ package ru.stqa.pft.addressbook;
 
 import org.testng.annotations.*;
 
-public class GroupCreationTests extends TestBase {
+public class GroupCreationTests  extends TestBase {
 
 
   @Test
   public void testGroupCreation()  {
-    gotoGroupPage();
+
+    goToGroupPage();
     initGroupCreation();
     fillGroupForm(new GroupData("test1", "test2", "test3"));
     submitGroupCreation();
