d69bad7f456a7eb8f55cb36f8efe1a6b1502bc5a
guschinav <sasha.gushchin.99@gmail.com>
2023-03-05 17:16:22 +0300

    Создаy общий базовый класс для классов-помощников

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -4,15 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 //помогает работать с меню
-public class NavigationHelper {
-    private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
 
     public NavigationHelper(FirefoxDriver wd) {
-        this.wd=wd;
+        super(wd);
     }
 
     public void goToGroupPage() {
-      wd.findElement(By.linkText("groups")).click();
+      click(By.linkText("groups"));
     }
 }
