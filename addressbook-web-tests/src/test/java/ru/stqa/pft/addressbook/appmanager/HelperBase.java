326a5a1be430c140fe104ce977e417a0355f4c5e
guschinav <sasha.gushchin.99@gmail.com>
2023-03-15 19:38:27 +0300

    Оптимизация заполнения форм: оставляем не изменяющися значения в полях

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -20,6 +20,8 @@ public class HelperBase {
     public void type(By locator, String text) {
         click(locator);
         if (text != null){
+            String existingText = wd.findElement(locator).getAttribute("value");
+            if (! text.equals(existingText)) {
             wd.findElement(locator).clear();
             wd.findElement(locator).sendKeys(text);
         }
