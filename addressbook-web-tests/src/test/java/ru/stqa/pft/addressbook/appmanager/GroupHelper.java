b81c76a89b34d9de3afb9dcb0e33be342335364d
guschinav <sasha.gushchin.99@gmail.com>
2023-03-18 16:05:16 +0300

    Реализована проверка и обеспечение выполнения предусловий

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -49,4 +49,16 @@ public class GroupHelper extends HelperBase {
     public void deleateGroup() {
         click(By.name("delete"));
     }
+
+    public void createGroup(GroupData group) {
+        initGroupCreation();
+        fillGroupForm(group);
+        submitGroupCreation();
+        returnToGroupPage();
+
+    }
+
+    public boolean isThereAGroup() {
+        return isElementPresent(By.name("selected[]"));
+    }
 }
