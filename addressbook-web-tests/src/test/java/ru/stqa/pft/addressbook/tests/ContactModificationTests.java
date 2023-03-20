49d3c9282973138bdb6a4d85607912e030390565
guschinav <sasha.gushchin.99@gmail.com>
2023-03-21 00:27:26 +0300

    Update ContactModificationTests.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -12,7 +12,7 @@ public class ContactModificationTests extends TestBase{
         app.getNavigationHelper().goToHomePage();
         if (! app.getContactHelper().isThereAContact()) {
             app.getNavigationHelper().goToNewAddPage();
-            app.getContactHelper().createContact(new ContactData("John", "Silver", "Yellow", "+791115648594", "test@mail.ru", "New Orenburg", "test1"), true);
+            app.getContactHelper().createContact(new ContactData("John", "Silver", "Yellow", "+791115648594", "test@mail.ru", "New Orenburg", "test1"));
         }
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillNewContact(new ContactData("Tom", "Gold", "Yellow", "+111111111111", "test@mail.ru", "New Orenburg", null), false);
