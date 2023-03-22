620e740eaa42275e3bbfaa87c3756eca420bc1ba
guschinav <sasha.gushchin.99@gmail.com>
2023-03-22 09:38:32 +0300

    Изменено предусловие в тестах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,7 +1,9 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactModificationTests extends TestBase{
 
@@ -10,9 +12,15 @@ public class ContactModificationTests extends TestBase{
     public void testContactModification () {
 
         app.getNavigationHelper().goToHomePage();
-        if (! app.getContactHelper().isThereAContact()) {
+        if (!app.getContactHelper().isThereAContact()) {
+            app.getNavigationHelper().goToGroupPage();
+            if (!app.getGroupHelper().isThereAGroup()) {
+                app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+            }
             app.getNavigationHelper().goToNewAddPage();
-            app.getContactHelper().createContact(new ContactData("John", "Silver", "Yellow", "+791115648594", "test@mail.ru", "New Orenburg", "test1"));
+            String CurrentGroup = app.wd.findElement(By.xpath("//div[@id='content']/form/select[5]/option[2]")).getText();
+            app.getContactHelper().createContact(new ContactData("John", "Silver", "Yellow", "+791115648594", "test@mail.ru", "New Orenburg", CurrentGroup));
+            app.getNavigationHelper().goToHomePage();
         }
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillNewContact(new ContactData("Tom", "Gold", "Yellow", "+111111111111", "test@mail.ru", "New Orenburg", null), false);
