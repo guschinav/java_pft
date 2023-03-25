5414f4edb7a97c8512c49e8da7d74e25347bae9c
guschinav <sasha.gushchin.99@gmail.com>
2023-03-25 22:32:29 +0300

    Лямбда-выражения (анонимные функции) и элементы функционального программирования

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -14,20 +14,14 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation()  {
     app.getNavigationHelper().goToGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();//количество групп до добавления
-    GroupData group = new GroupData("test99", null, null);
+    GroupData group = new GroupData("test2", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList(); //количество групп после добавления
     Assert.assertEquals(after.size(), before.size() + 1);
 
 
-    int max =0;
-    for (GroupData g: after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    
-    group.setId(max);
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after)); //преобразование списка в множество
   }
