1d7b1969f92c9b0e84a0e9a155b125fdae800b34
guschinav <sasha.gushchin.99@gmail.com>
2023-04-04 22:33:14 +0300

    Update ContactData.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -8,13 +8,68 @@ public class ContactData {
 
 
 
-    private  String firstname;
-    private  String middlename;
-    private  String lastname;
-    private  String mobile;
-    private  String email;
-    private  String address2;
+    private String firstname;
+    private String middlename;
+    private String lastname;
+    private String nickname;
+    private String company;
+    private String address;
+    private String mobile;
+    private String workphone;
     private String group;
+    private String homephone;
+    private String allPhones;
+    private String email;
+    private String email2;
+    private String email3;
+    private String allEmails;
+
+    public ContactData withAllEmails(String allEmails) {
+        this.allEmails = allEmails;
+        return this;
+    }
+
+    public ContactData withEmail3(String email3) {
+        this.email3 = email3;
+        return this;
+    }
+
+
+    public ContactData withEmail2(String email2) {
+        this.email2 = email2;
+        return this;
+    }
+
+    public ContactData withEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public ContactData withAllPhones(String allPhones) {
+        this.allPhones = allPhones;
+        return this;
+    }
+
+    public ContactData withHomePhone(String homephone) {
+        this.homephone = homephone;
+        return this;
+    }
+
+    public ContactData withWorkPhone(String workphone) {
+        this.workphone = workphone;
+        return this;
+    }
+
+    public ContactData withCompany(String company) {
+        this.company = company;
+        return this;
+    }
+
+    public ContactData withNickname(String nickname) {
+        this.nickname = nickname;
+        return this;
+    }
+
 
     public ContactData withGroup(String group) {
         this.group = group;
@@ -41,15 +96,13 @@ public class ContactData {
         return this;
     }
 
-    public ContactData withEmail(String email) {
-        this.email = email;
+
+
+    public ContactData withAddress(String address) {
+        this.address = address;
         return this;
     }
 
-    public ContactData withAddress2(String address2) {
-        this.address2 = address2;
-        return this;
-    }
 
     @Override
     public boolean equals(Object o) {
@@ -71,17 +124,19 @@ public class ContactData {
         return result;
     }
 
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "id='" + id + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+
     public ContactData withId(int id) {
         this.id = id;
         return this;
     }
-
-
-
-
-
-
-
 
     public int getId() {
         return id;
@@ -108,25 +163,50 @@ public class ContactData {
     public String getEmail() {
         return email;
     }
+    public String getWorkPhone() {
+        return workphone;
+    }
 
-    public String getAddress2() {
-        return address2;
+    public String getAddress() {
+        return address;
     }
 
     public String getGroup() {
         return group;
     }
 
-    @Override
-    public String toString() {
-        return "ContactData{" +
-                "id='" + id + '\'' +
-                ", firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                '}';
-    }
+
 
     public void setId(int id) {
         this.id = id;
+    }
+
+
+    public String getHomephone() {
+        return homephone;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAllPhones() {
+        return allPhones;
+    }
+
+    public String getEmail2() {
+        return email2;
+    }
+
+    public String getEmail3() {
+        return email3;
+    }
+
+    public String getAllEmails() {
+        return allEmails;
     }
 }
