22cc828f1331a8f585d9f05aac220e7dda7da2d6
guschinav <sasha.gushchin.99@gmail.com>
2023-04-09 18:31:38 +0300

    Лекция 6.11. Протоколирование (лог-файлы)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -2,6 +2,8 @@ package ru.stqa.pft.addressbook.tests;
 
 
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
@@ -17,6 +19,8 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+
+
 
   @DataProvider
   public Iterator<Object[]> validGroups () throws IOException {
